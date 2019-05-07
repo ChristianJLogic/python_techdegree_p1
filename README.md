83bda906d3e6e624f940c7ccca87d2a2e6fc390f
Your name here <christianjereljohnson@gmail.com>
2019-05-07 17:43:00 -0500

    Finished Project1 Submitting

--- README.md
+++ README.md
@@ -1,8 +1,15 @@
 # python_techdegree_p1
+Began Project: May 4th, 2019
 
 Project Overview: A simple console guessing game that allows the user to pick a number between x number and tells the user if there guess is correct, too high, or too low.
 
 Project Trello Board: https://trello.com/b/Iw078A7b/python-techdegree-project-1
+
+Biggest challenge: Finding out how to get high score to work and display correctly.
+
+How it can be improved: I think once I learn more about dictionaries the high score could be stored in it and given extra functionality
+
+submitted May 7th, 2019
 
 
 
--- guessing_game.py
+++ guessing_game.py
@@ -20,10 +20,19 @@ def welcome():
 
 welcome()
 
+high_score_container = []
+high_score_user_container = []
 
-def start_game():
 
-    # Difficultly setting
+def start_game(high_score, high_score_user):
+
+    if bool(high_score) is True and bool(high_score_user) is True:
+        print(f"High Score: {high_score[-1]}|{high_score_user[-1]}")
+
+    # User attempts
+    user_attempts = 0
+
+    # Difficultly and High Score setting
     diff_setting = input("Choose a difficultly setting: [E]asy, [M]edium, [H]ard, [C]razy\n> ").upper()
     if diff_setting == 'E':
         difficultly = 10
@@ -41,6 +50,7 @@ def start_game():
 
     # Game Mechanic
     while True:
+
         try:
             player_guess = int(input("What number am I thinking of?\n> "))
 
@@ -49,22 +59,42 @@ def start_game():
                 continue
             elif answer < player_guess:
                 print("Guess it too high!")
+                user_attempts += 1
                 continue
             elif answer > player_guess:
                 print("Guess is too low!")
+                user_attempts += 1
                 continue
             elif answer == player_guess:
+                user_attempts += 1
                 print("Wow you've guessed right! Good Job!\n")
+
+                # Displaying attempts
+                print(f"You got the correct number in {user_attempts} attempts")
+
+                # High score Mechanic
+                if bool(high_score) is False and bool(high_score_user) is False:
+                    high_score.append(user_attempts)
+                    user_initials = input("What are your initials?\n> ")
+                    high_score_user.append(user_initials)
+                elif high_score[-1] > user_attempts:
+                    print("You beat the high score!")
+                    user_initials = input("What are your initials?\n> ")
+                    high_score.append(user_attempts)
+                    high_score_user.append(user_initials)
+
+                # Would you like to play again
                 play_again = input("\nWould you like to play again? [Y]es/[N]o\n> ").upper()
 
                 if play_again == 'Y':
-                    start_game()
+                    start_game(high_score_container, high_score_user_container)
                 elif play_again == 'N':
                     print("Thank you for playing the Guessing Game! Good-Bye.")
                     break
                 else:
-                    print("Invalid choice: Try again\n")
-                    continue
+                    print("Invalid choice: Closing game thanks for player. Good-Bye.")
+                    break
+
         except ValueError:
             print("INVALID: Guess must be a number try again.\n")
             continue
@@ -72,4 +102,4 @@ def start_game():
 
 if __name__ == '__main__':
     # Kick off the program by calling the start_game function.
-    start_game()
+    start_game(high_score_container, high_score_user_container)
